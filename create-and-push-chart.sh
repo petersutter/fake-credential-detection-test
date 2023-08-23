6201d82ab22ac57a4178ded3fa3cf3bd37c98f9a
Peter Sutter <peter.sutter@sap.com>
2023-08-23 12:48:05 +0200

    check if dummy cred. is detected

---
+++ create-and-push-chart.sh
@@ -0,0 +1,8 @@
+# this file does not contain real credentials
+# Get an access token:
+# gcloud auth login
+# gcloud auth print-access-token
+#ACCESS_TOKEN=<your access token>
+ACCESS_TOKEN=ya29.a0AbVbY1QqzjdMHKE40USI0Zb68pw8XG-tYs2BCgBk_77SKzoznr5vrGulgYDXRNuc9t5DbV-tno30p4XcmxaCDAXkrqdahdJoIjnakV1L2NvKEh7zSOsJYjBoAXJPt3Fvn7YIxF-EHX2J1ewsi47-Pm9u3fl-pZd5mWi4VgYaCgYKAY0SARISFQFWKvPl_KpdV1uDpHwqYJ7itcfd4Q0128
+ACCESS_TOKEN=<your access token>     # <-------------------------- replace with your access token
+COMPONENT_DIR="$(dirname $0)/.."
